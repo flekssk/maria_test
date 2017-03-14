d02980a7679f75ec012d2f55e3f94b19fce73868
flekssk <fleks-sk@yandex.ru>
2017-03-14 11:42:43 +0400

    Admin module added

--- config/web.php
+++ config/web.php
@@ -46,6 +46,11 @@ $config = [
             ],
         ],
     ],
+    'modules' => [
+        'admin' => [
+            'class' => 'app\modules\admin\Module',
+        ],
+    ],
     'params' => $params,
 ];
 
