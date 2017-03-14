421ab8cbd4ae0c47716ec73a5e3a4f23c1212f54
flekssk <fleks-sk@yandex.ru>
2017-03-14 23:28:57 +0400

    Change titles

--- modules/admin/views/post/index.php
+++ modules/admin/views/post/index.php
@@ -15,7 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Post', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Создать Пост', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
